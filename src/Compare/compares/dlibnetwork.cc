410518884e6a45887659448cc47ae086ba257e15
Grzegorz Matczak <grzegorz.matczak@unikie.com>
2020-12-03 11:21:45 +0100

    Fix problem witch compare method

--- src/Compare/compares/dlibnetwork.cc
+++ src/Compare/compares/dlibnetwork.cc
@@ -17,18 +17,67 @@ void Compares::DlibNetwork::alertBadImage(const cv::Mat_<uchar> &image, QString 
 void Compares::DlibNetwork::process(std::vector<_postData> &_data)
 {
   Logger->trace("DlibNetwork");
+  int m_width = _data[0].processing.cols;
+  int m_height = _data[0].processing.rows;
+
+  const cv::Mat_<uchar> binary = _data[0].processing.clone();
+  const cv::Mat_<uchar> gt = _data[1].processing.clone();
+
+#if (DEBUG)
+  Logger->critical("DlibNetwork  m_width:{}", m_width);
+  Logger->critical("DlibNetwork  m_height:{}", m_height);
+  cv::imshow("binary", _data[0].processing);
+  cv::imshow("gt", _data[1].processing);
+  cv::waitKey(0);
+#endif
+  struct imageErrors m_errors2 { 0, 0, 0, 0, 0 };
+
+
+
+  for (int i = 0; i < m_width; i++) {
+#if (DEBUG)
+    Logger->trace("DlibNetwork:{}", i);
+#endif
+    for (int j = 0; j < m_height; j++) {
+
+      if (_data[0].processing.at<unsigned char>(cv::Point(i, j)) > 0) {   // Binary   - true
+        if (_data[1].processing.at<unsigned char>(cv::Point(i, j)) > 0) { // Gt       - true
+          m_errors2.tpError += 1;
+        } else { // Gt       - false
+          m_errors2.fpError += 1;
+        }
+      } else {                                                            // Binary   - false
+        if (_data[1].processing.at<unsigned char>(cv::Point(i, j)) > 0) { // Gt       - true
+          m_errors2.fnError += 1;
+        } else { // Gt       - false
+          m_errors2.tnError += 1;
+        }
+      }
+    }
+  }
+
+#if (DEBUG)
+  Logger->info("DlibNetwork tpError:{}", m_errors2.tpError);
+  Logger->info("DlibNetwork fpError:{}", m_errors2.fpError);
+  Logger->info("DlibNetwork fnError:{}", m_errors2.fnError);
+  Logger->info("DlibNetwork tnError:{}", m_errors2.tnError);
+#endif
+  _data[0].ie = m_errors2;
+}
+
+/*
+void Compares::DlibNetwork::process2(std::vector<_postData> &_data)
+{
+  Logger->trace("DlibNetwork");
   const cv::Mat_<uchar> binary = _data[0].processing.clone();
   const cv::Mat_<uchar> gt = _data[1].processing.clone();
 
   #if (DEBUG)
       cv::imshow("binary", _data[0].processing);
       cv::imshow("gt", _data[1].processing);
-      cv::waitKey(1);
+      cv::waitKey(0);
   #endif
-  /*
-  cv::imshow("0", binary);
-  cv::imshow("1", gt);
-  cv::waitKey(0);*/
+
 
   cv::MatConstIterator_<uchar> itBinary = binary.begin();
   cv::MatConstIterator_<uchar> itGT = gt.begin();
@@ -69,7 +118,11 @@ void Compares::DlibNetwork::process(std::vector<_postData> &_data)
   Logger->info("DlibNetwork tnError:{}", m_errors2.tnError);
 #endif
   _data[0].ie = m_errors2;
- /// Logger->trace("imageErrors Compare::CodeStats2014 done");
-  //return m_errors2;
 }
+*/
+
+
+
+
+
 void Compares::DlibNetwork::endProcess(std::vector<_postData> &_data) {}
